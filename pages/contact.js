d7a29b0d2da5bef2b49ac75476cc1cbcaf089790
Sam Goddard <hello@samgoddard.co.uk>
2021-05-25 22:39:47 +0100

    motion fade added to contact illos

--- pages/contact.js
+++ pages/contact.js
@@ -27,34 +27,34 @@ export default function Contact() {
         className="bg-blue-dark bg-noise text-white overflow-hidden min-h-screen flex flex-wrap py-12 md:pt-24 md:pb-[14vh] 2xl:pt-32 2xl:pb-[17vh]"
       >
         {/* Lady With Megaphone */}
-        <div className="w-[44%] md:w-[38%] lg:w-[34%] xl:w-[30%] 2xl:w-[480px] absolute bottom-0 right-0 z-10 mr-[-18%] mb-[-5%] md:mr-[-10%] 2xl:mr-[-150px]">
+        <motion.div variants={fade} className="w-[44%] md:w-[38%] lg:w-[34%] xl:w-[30%] 2xl:w-[480px] absolute bottom-0 right-0 z-10 mr-[-18%] mb-[-5%] md:mr-[-10%] 2xl:mr-[-150px]">
           <Image height={800} width={415} src="/icons/woman-megaphone.svg" alt="Woman With Megaphone Illustration" layout="responsive" priority />
-        </div>
+        </motion.div>
 
         {/* Bottom Middle Bigger Leaf */}
-        <div className="w-[45%] md:w-[40%] xl:w-[35%] 2xl:w-[35%] absolute bottom-0 right-0 ml-[-39%] md:mr-[10%] xl:mr-[2%] 2xl:ml-[-19%] mb-[-26%] md:mb-[-24%] xl:mb-[-21%] 2xl:mb-[-20%] z-0 transform -rotate-45">
+        <motion.div variants={fade} className="w-[45%] md:w-[40%] xl:w-[35%] 2xl:w-[35%] absolute bottom-0 right-0 ml-[-39%] md:mr-[10%] xl:mr-[2%] 2xl:ml-[-19%] mb-[-26%] md:mb-[-24%] xl:mb-[-21%] 2xl:mb-[-20%] z-0 transform -rotate-45">
           <Image width={551} height={555} layout="responsive" src="/icons/plant-3.svg" alt="Plant Illustration" className="w-full transform -rotate-90" priority />
-        </div>
+        </motion.div>
 
         {/* Middle Right Monsterra */}
-        <div className="w-[33%] md:w-[24%] xl:w-[20%] 2xl:w-[320px] absolute top-0 right-0 mr-[-14%] md:mr-[-10%] 2xl:mr-[-140px] mt-[35vh] md:mt-[16vh] xl:mt-[0%] z-0">
+        <motion.div variants={fade} className="w-[33%] md:w-[24%] xl:w-[20%] 2xl:w-[320px] absolute top-0 right-0 mr-[-14%] md:mr-[-10%] 2xl:mr-[-140px] mt-[35vh] md:mt-[16vh] xl:mt-[0%] z-0">
           <Image width={278} height={276} layout="responsive" src="/icons/plant-2.svg" alt="Plant Illustration" className="w-full transform -rotate-45" priority />
-        </div>
+        </motion.div>
 
         {/* Middle Leaf Monsterra */}
-        <div className="w-[35%] md:w-[38%] xl:w-[25%] 2xl:w-[22%] absolute bottom-0 left-0 ml-[-18%] md:ml-[-25%] xl:ml-[-18%] 2xl:ml-[-12%] mb-[70%] md:mb-[40%] xl:mb-[35%] 2xl:mb-[35%] z-0">
+        <motion.div variants={fade} className="w-[35%] md:w-[38%] xl:w-[25%] 2xl:w-[22%] absolute bottom-0 left-0 ml-[-18%] md:ml-[-25%] xl:ml-[-18%] 2xl:ml-[-12%] mb-[70%] md:mb-[40%] xl:mb-[35%] 2xl:mb-[35%] z-0">
           <Image width={278} height={276} layout="responsive" src="/icons/plant-2.svg" alt="Plant Illustration" className="w-full transform -rotate-180" priority />
-        </div>
+        </motion.div>
 
         {/* Bottom Left Bigger Leaf */}
-        <div className="w-[65%] md:w-[44%] xl:w-[38%] 2xl:w-[35%] absolute bottom-0 left-0 ml-[-39%] md:ml-[-25%] xl:ml-[-20%] 2xl:ml-[-19%] mb-[0%] md:mb-[3%] xl:mb-[4%] 2xl:mb-[2%] z-0">
+        <motion.div variants={fade} className="w-[65%] md:w-[44%] xl:w-[38%] 2xl:w-[35%] absolute bottom-0 left-0 ml-[-39%] md:ml-[-25%] xl:ml-[-20%] 2xl:ml-[-19%] mb-[0%] md:mb-[3%] xl:mb-[4%] 2xl:mb-[2%] z-0">
           <Image width={551} height={555} layout="responsive" src="/icons/plant-3.svg" alt="Plant Illustration" className="w-full transform -rotate-12" priority />
-        </div>
+        </motion.div>
 
         {/* Bottom Left Leaf */}
-        <div className="w-[45%] md:w-[30%] xl:w-[28%] 2xl:w-[28%] absolute bottom-0 left-0 ml-[-20%] md:ml-[-15%] xl:ml-[-13%] 2xl:ml-[-12%] mb-[-10%] md:mb-[-7%] xl:mb-[-8%] 2xl:mb-[-10%] z-0">
+        <motion.div variants={fade} className="w-[45%] md:w-[30%] xl:w-[28%] 2xl:w-[28%] absolute bottom-0 left-0 ml-[-20%] md:ml-[-15%] xl:ml-[-13%] 2xl:ml-[-12%] mb-[-10%] md:mb-[-7%] xl:mb-[-8%] 2xl:mb-[-10%] z-0">
           <Image width={267} height={253} layout="responsive" src="/icons/plant-4.svg" alt="Plant Illustration" className="w-full transform" priority />
-        </div>
+        </motion.div>
 
         <motion.div variants={fade} className="relative z-30 flex flex-wrap w-full">
           <div className="mb-auto w-full">
