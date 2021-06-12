22579dd808f86b694a0927ef6756356382a1dea4
Sam Goddard <hello@samgoddard.co.uk>
2021-06-12 22:08:28 +0100

    preview mode banner tidied up

--- pages/_app.js
+++ pages/_app.js
@@ -47,7 +47,7 @@ export default function App({ Component, pageProps }) {
         }}
       />
       
-      { pageProps.preview && <><div className={'fixed bottom-0 w-full p-1 bg-red bg-opacity-100 text-white justify-center flex z-30'}>Preview Mode - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a></div></> }
+      { pageProps.preview && <><div className={'fixed bottom-0 w-full p-2 bg-red-500 bg-opacity-100 text-white justify-center flex z-50 uppercase font-medium'}>! Preview Mode Enabled - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a> !</div></> }
       
       <AnimatePresence exitBeforeEnter initial={false}>
         <Component {...pageProps} key={router.asPath} />
