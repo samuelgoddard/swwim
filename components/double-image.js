123a1da3af867800612a4099e8d2cbcc187dbf3b
samuelgoddard <hello@samgoddard.co.uk>
2023-09-26 10:13:53 +0100

    preview mode

--- components/double-image.js
+++ components/double-image.js
@@ -2,25 +2,33 @@ import ImageWrapper from '../helpers/image-wrapper'
 
 export default function DoubleImage({ image1, image2 }) {
   return (
-    <div className="w-full single-image">
-      <div className="flex flex-wrap -mx-4 md:-mx-6">
-        <div className="w-1/2 px-4 md:px-6">
-          <ImageWrapper
-            image={image1}
-            className="w-full"
-            baseWidth={740}
-            baseHeight={740}
-          />
+    <>
+      {(image1 || image2) && (
+        <div className="w-full single-image">
+          <div className="flex flex-wrap -mx-4 md:-mx-6">
+            {image1 && (
+              <div className="w-1/2 px-4 md:px-6">
+                <ImageWrapper
+                  image={image1}
+                  className="w-full"
+                  baseWidth={740}
+                  baseHeight={740}
+                />
+              </div>
+            )}
+            {image2 && (
+              <div className="w-1/2 px-4 md:px-6">
+                <ImageWrapper
+                  image={image2}
+                  className="w-full"
+                  baseWidth={740}
+                  baseHeight={740}
+                />
+              </div>
+            )}
+          </div>
         </div>
-        <div className="w-1/2 px-4 md:px-6">
-          <ImageWrapper
-            image={image2}
-            className="w-full"
-            baseWidth={740}
-            baseHeight={740}
-          />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
