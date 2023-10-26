4401bab98154f30a970dc095d1bb547ba461665c
samuelgoddard <hello@samgoddard.co.uk>
2023-10-26 09:35:02 +0100

    case teaser fallback iage

---
+++ helpers/seo.js
@@ -0,0 +1,24 @@
+export default {
+  defaultTitle: 'Social, Digital & Content Creation | Swwim',
+  description: 'We specialise in influencer marketing, social media, content creation and copywriting. Our experience spans lifestyle, health and beauty and home and interiors.',
+  titleTemplate: '%s | Swwim',
+  openGraph: {
+    type: 'website',
+    locale: 'en_GB',
+    url: 'https://www.weswwim.com/',
+    site_name: 'Swwim',
+    images: [
+      {
+        url: 'https://weswwim.com/images/social-share.jpg',
+        width: 1200,
+        height: 630,
+        alt: `Swwim Logo`,
+      },
+    ],
+  },
+  twitter: {
+    handle: '@weswwim',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Logo from '../components/logo'
 import Image from 'next/image'
+import SEO from '../helpers/seo';
 import LottieTest from '../components/lottie-test'
 import {AlertContext} from '../contexts/alert'
 import {NewsletterContext} from '../contexts/newsletter'
@@ -98,30 +99,8 @@ export default function App({ Component, pageProps }) {
           `,
         }}
       />
-
-      <DefaultSeo
-        titleTemplate = "%s | Swwim"
-        defaultTitle="Social, Digital &amp; Content Creation"
-        description="We specialise in influencer marketing, social media, content creation and copywriting. Our experience spans lifestyle, health and beauty and home and interiors."
-        openGraph={{
-          type: 'website',
-          locale: 'en_GB',
-          url: 'https://www.weswwim.com/',
-          site_name: 'Swwim',
-          images: [
-            {
-              url: '/images/social-share.jpg',
-              width: 800,
-              height: 600
-            },
-          ]
-        }}
-        twitter={{
-          handle: '@weswwim',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
-      />
+      
+      <DefaultSeo {...SEO} />
       
       { pageProps.preview && <><div className={'fixed bottom-0 w-full p-2 bg-red-500 bg-opacity-100 text-white justify-center flex z-50 uppercase font-medium'}>! Preview Mode Enabled - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a> !</div></> }
 
